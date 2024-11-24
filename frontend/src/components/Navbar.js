179bbc7b43eed7b49de132761b942d485b2f6894
Sathmi1234 <devsadini@gmail.com>
2024-11-24 13:50:18 +0530

    Signup page created

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,7 @@ const Navbar = () => {
       <Link to="/" style={styles.link}>Home</Link>
       <Link to="/about-us" style={styles.link}>About Us</Link>
       <Link to="/contact-us" style={styles.link}>Contact Us</Link>
+      <Link to="/sign-up" style={styles.link}>Sign up</Link>
     </nav>
   );
 };
