05dd337905d2bade7c99a4c36b2980f65ab42725
Sathmi1234 <devsadini@gmail.com>
2024-12-02 01:35:45 +0530

    Components created for sidebar headings

---
+++ frontend/src/components/dashboard/student/EnrolledSubjects.js
@@ -0,0 +1,15 @@
+import Sidebar from "../Sidebar";
+
+export default function EnrolledSubjects(){
+    return(
+        <div className="flex min-h-screen bg-gray-100">
+        <div className="w-64 bg-richblue-800 border-r border-richblack-700">
+        <br/><br/>
+          <Sidebar />
+        </div>
+        <div className="flex-1 p-8"><br/><br/>
+          <h1 className="text-3xl font-bold text-gray-800 mb-4">Enrolled Subjects</h1>
+        </div>
+      </div>
+    )
+}
