c72a14007247ab68b8023df7e0c7e5abe4fddaca
Sathmi1234 <devsadini@gmail.com>
2024-12-01 23:48:19 +0530

    created dashboard links

--- frontend/src/components/dashboard/Sidebar.js
+++ frontend/src/components/dashboard/Sidebar.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import {sidebarLinks as LINKS} from "../../data/dashboard-links";
+import SidebarLinks from "./SideBarLinks";
+//import { useNavigate } from "react-router-dom";
+import { VscSignOut } from "react-icons/vsc";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import { IoMdCloseCircle } from "react-icons/io";
 
-function Sidebar() {
-  return (
-    <div>
-      <h1>Hii</h1>
-    </div>
-  )
+const Sidebar = () => {
+    const [clicked, setClicked] = useState(false);
+    //const naviagte = useNavigate ();
+    const accountType ='student'
+    return(
+        <>
+            <div className={`flex transition-all ease-out duration-200 min-w-[222px] flex-col border-r-[1px]  border-richblack-700  h-[100%] bg-richblue-800 py-10
+            absolute z-[5000] ${clicked?  "left-0" : "left-[-222px]" }
+            md:relative md:left-0 
+             `} >
+                <p 
+                onClick={() => setClicked(prev => !prev)}
+                className="absolute  text-[25px] visible md:hidden top-2 right-[-20px] z-[1000] text-yellow-100" >{!clicked ? <div className="animation animate-pulse duration-100"><FaArrowAltCircleRight /></div> : <IoMdCloseCircle /> }</p>
+                
+
+
+                <div className="flex flex-col">
+                    {
+                        LINKS.map((link, index)=>{
+                            if(accountType !== link.type){
+                                console.log("link.type and user?.accountType", link.type, accountType);
+                                console.log("link.type", link.type);
+                                return null;
+                            }
+                            console.log("link=", link);
+                            return(
+                              <SidebarLinks setClicked={setClicked} key={link?.id} iconName={link?.icon} link={link} />
+                            )
+                        })
+                    }
+                </div>
+
+                <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-600 " >
+                    <div className="flex flex-col">
+                        <SidebarLinks link={{name:"Settings", path:"dashboard/settings"}}  iconName="VscSettingsGear"/>
+                    </div>
+
+                    <button className="text-sm font-medium text-richblack-300 "> Logout </button>
+
+                    <div className="flex text-center text-[13px] px-6 py-2 hover:cursor-pointer hover:scale-95 transition-all duration-200 rounded-md font-bold bg-yellow-50 text-black items-center gap-x-2 justify-center">
+                        <VscSignOut className="text-lg" />
+                        <span>Logout</span>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default Sidebar
+export default Sidebar ;
---
+++ frontend/src/data/dashboard-links.js
@@ -0,0 +1,43 @@
+export const sidebarLinks = [
+  {
+    id: 1,
+    name: "My Profile",
+    path: "/dashboard/my-profile",
+    icon: "VscAccount",
+  },
+  {
+    id: 2,
+    name: "Dashboard",
+    path: "/dashboard/instructor",
+    type: 'tutor',
+    icon: "VscDashboard",
+  },
+  {
+    id: 3,
+    name: "My Courses",
+    path: "/dashboard/my-courses",
+    type: 'tutor',
+    icon: "VscVm",
+  },
+  {
+    id: 4,
+    name: "Add Course",
+    path: "/dashboard/add-course",
+    type: 'tutor',
+    icon: "VscAdd",
+  },
+  {
+    id: 5,
+    name: "Enrolled Courses",
+    path: "/dashboard/enrolled-courses",
+    type: 'student',
+    icon: "VscMortarBoard",
+  },
+  {
+    id: 6,
+    name: "Your Cart",
+    path: "/dashboard/cart",
+    type: 'student',
+    icon: "VscHistory",
+  },
+];
