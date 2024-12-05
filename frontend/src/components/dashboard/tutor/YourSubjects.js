727450537ee4b84d7172df637a89b2f616ba0b13
Sathmi1234 <devsadini@gmail.com>
2024-12-05 15:29:41 +0530

    fixed some styles

--- frontend/src/components/dashboard/tutor/YourSubjects.js
+++ frontend/src/components/dashboard/tutor/YourSubjects.js
@@ -4,10 +4,10 @@ export default function YourSubjects(){
     return(
         <div className="flex min-h-screen bg-gray-100">
         <div className="w-64 bg-richblue-800 border-r border-richblack-700">
-        <br/><br/>
+        
           <Sidebar />
         </div>
-        <div className="flex-1 p-8"><br/><br/>
+        <div className="flex-1 p-8">
           <h1 className="text-3xl font-bold text-gray-800 mb-4">Your Subjects</h1>
         </div>
       </div>
