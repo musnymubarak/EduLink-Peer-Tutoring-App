7fdef27c446d3110084c799d33485f23283b1d39
Sathmi1234 <devsadini@gmail.com>
2024-12-05 15:29:18 +0530

    Changed existing paths and added new paths

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -24,7 +24,7 @@ export default function Login() {
   const handleLogin = (event) => {
     event.preventDefault(); 
     //login logic 
-    navigate("/dashboard"); 
+    navigate("/dashboard/student"); 
   };
 
   return (
