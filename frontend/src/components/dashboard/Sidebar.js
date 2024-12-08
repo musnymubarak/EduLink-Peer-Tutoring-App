f1ed0602eec986144331376a2cc036e4ff32a916
Sathmi1234 <devsadini@gmail.com>
2024-12-08 14:29:06 +0530

    Removed settings function

--- frontend/src/components/dashboard/Sidebar.js
+++ frontend/src/components/dashboard/Sidebar.js
@@ -7,7 +7,7 @@ import { IoMdCloseCircle } from "react-icons/io";
 
 const Sidebar = () => {
     const [clicked, setClicked] = useState(false);
-    const accountType ='student'
+    const accountType ='tutor'
     return(
         <>
             <div className={`flex transition-all ease-out duration-200 min-w-[222px] flex-col border-r-[1px]  border-richblack-700  h-[100%] bg-richblue-800 py-10
@@ -37,10 +37,6 @@ const Sidebar = () => {
                 </div>
 
                 <div className="mx-auto mt-6 mb-6 h-[1px] w-10/12 bg-richblack-600 " >
-                    <div className="flex flex-col">
-                        <SidebarLinks link={{name:"Settings", path:"dashboard/settings"}}  iconName="VscSettingsGear"/>
-                    </div>
-
                     <button className="text-sm font-medium text-richblack-300 "/>
 
                     <div className="flex text-center text-[13px] px-6 py-2 hover:cursor-pointer hover:scale-95 transition-all duration-200 rounded-md font-bold bg-red-500 text-black items-center gap-x-2 justify-center">
