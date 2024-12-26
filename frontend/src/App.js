f9da194e470bcf4f9efdc95cbe54c9295b7c48dc
Disitha <disithar@gmail.com>
2024-12-26 23:55:21 +0530

    create ExploreSubject file for tutor (Routes)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,6 +21,7 @@ import TProfile from './components/dashboard/tutor/TProfile';
 import TSubjects from './components/dashboard/tutor/TSubjects';
 import TSchedule from './components/dashboard/tutor/TSchedule';
 import TSubjectDetails from './components/dashboard/tutor/TSubjectDetails';
+import ExploreSubjects from './components/ExploreSubjects';
 
 import AdminDashboard from './components/dashboard/admin/AdminDashboard';
 import DashboardOverview from './components/dashboard/admin/Pages/DashboardOverview';
@@ -45,44 +46,86 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
-        <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/explore' element={<ExploreCourses/>}/>
-        <Route path='/explore/:id' element={<ExploreSingleCourse/>}/>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/explore" element={<ExploreCourses />} />
+        <Route path="/explore/:id" element={<ExploreSingleCourse />} />
 
         {/*student*/}
-        <Route path='/dashboard/student/' element={<Dashboard/>}/>
-        <Route path='/dashboard/student/profile' element={<Profile/>}/>
-        <Route path='/dashboard/student/subjects' element={<Subjects/>}/>
-        <Route path='/dashboard/student/schedule' element={<Schedule/>}/>
-        <Route path='/dashboard/student/enrolled-courses' element={<EnrolledSubjects/>}/>
-        <Route path='/dashboard/student/resources' element={<Resources/>}/>
-        <Route path="/dashboard/student/subject/:id" element={<SubjectDetails />} />
+        <Route path="/dashboard/student/" element={<Dashboard />} />
+        <Route path="/dashboard/student/profile" element={<Profile />} />
+        <Route path="/dashboard/student/subjects" element={<Subjects />} />
+        <Route path="/dashboard/student/schedule" element={<Schedule />} />
+        <Route
+          path="/dashboard/student/enrolled-courses"
+          element={<EnrolledSubjects />}
+        />
+        <Route path="/dashboard/student/resources" element={<Resources />} />
+        <Route
+          path="/dashboard/student/subject/:id"
+          element={<SubjectDetails />}
+        />
 
         {/*tutor*/}
-        <Route path='/dashboard/tutor' element={<TDashboard/>}/>
-        <Route path='/dashboard/tutor/profile' element={<TProfile/>}/>
-        <Route path='/dashboard/tutor/subjects' element={<TSubjects/>}/>
-        <Route path='/dashboard/tutor/schedule' element={<TSchedule/>}/>
-        <Route path="/dashboard/tutor/subject/:id" element={<TSubjectDetails/>} />
-        <Route path='/dashboard/tutor/requests' element={<Requests/>}/>
-        <Route path='/dashboard/tutor/your-subjects' element={<YourSubjects/>}/>
+        <Route path="/explore-subjects" element={<ExploreSubjects />} />
+        <Route path="/dashboard/tutor" element={<TDashboard />} />
+        <Route path="/dashboard/tutor/profile" element={<TProfile />} />
+        <Route path="/dashboard/tutor/subjects" element={<TSubjects />} />
+        <Route path="/dashboard/tutor/schedule" element={<TSchedule />} />
+        <Route
+          path="/dashboard/tutor/subject/:id"
+          element={<TSubjectDetails />}
+        />
+        <Route path="/dashboard/tutor/requests" element={<Requests />} />
+        <Route
+          path="/dashboard/tutor/your-subjects"
+          element={<YourSubjects />}
+        />
 
         {/* admin */}
-        <Route path='/dashboard/admin' element={<AdminDashboard/>}/>
-        <Route path='/dashboard/admin/Pages/DashboardOverview' element={<DashboardOverview/>}/>
-        <Route path='/dashboard/admin/Pages/ManageSubjects' element={<ManageSubjects/>}/>
-        <Route path='/dashboard/admin/Pages/ManageStudents' element={<ManageStudents/>}/>
-        <Route path='/dashboard/admin/Pages/ManageTutors' element={<ManageTutors/>}/>
-        <Route path='/dashboard/admin/Pages/NotificationPage' element={<NotificationPage/>}/>
+        <Route path="/dashboard/admin" element={<AdminDashboard />} />
+        <Route
+          path="/dashboard/admin/Pages/DashboardOverview"
+          element={<DashboardOverview />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/ManageSubjects"
+          element={<ManageSubjects />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/ManageStudents"
+          element={<ManageStudents />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/ManageTutors"
+          element={<ManageTutors />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/NotificationPage"
+          element={<NotificationPage />}
+        />
 
-        <Route path='/dashboard/admin/Pages/AddStudent' element={<AddStudent/>}/>
-        <Route path='/dashboard/admin/Pages/ListStudent' element={<ListStudent/>}/>
-        <Route path='/dashboard/admin/Pages/AddSubject' element={<AddSubject/>}/>
-        <Route path='/dashboard/admin/Pages/ListSubject' element={<ListSubject/>}/>
-        <Route path='/dashboard/admin/Pages/AddTutor' element={<AddTutor/>}/>
-        <Route path='/dashboard/admin/Pages/ListTutor' element={<ListTutor/>}/>
-        
+        <Route
+          path="/dashboard/admin/Pages/AddStudent"
+          element={<AddStudent />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/ListStudent"
+          element={<ListStudent />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/AddSubject"
+          element={<AddSubject />}
+        />
+        <Route
+          path="/dashboard/admin/Pages/ListSubject"
+          element={<ListSubject />}
+        />
+        <Route path="/dashboard/admin/Pages/AddTutor" element={<AddTutor />} />
+        <Route
+          path="/dashboard/admin/Pages/ListTutor"
+          element={<ListTutor />}
+        />
       </Routes>
     </Router>
   );
