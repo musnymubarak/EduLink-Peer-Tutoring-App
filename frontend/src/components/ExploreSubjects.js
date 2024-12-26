0d66df40f2cc7fe9589bc558d7d94ce7f603602e
Disitha <disithar@gmail.com>
2024-12-27 00:01:53 +0530

    add usestate

--- frontend/src/components/ExploreSubjects.js
+++ frontend/src/components/ExploreSubjects.js
@@ -2,3 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
+export default function ExploreSubjects() {
+
+    const [subjects, setSubjects] = useState([]); 
+    const [loading, setLoading] = useState(true); 
+    const [error, setError] = useState(null); 
+
+    const navigate = useNavigate();
+
+
+}
