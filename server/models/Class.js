abacfe6cfbacec49bb4b4cca273008f9cf588cf6
musnymubarak <musnymohammed@gmail.com>
2025-01-12 22:52:31 +0530

    Class model created

---
+++ server/models/Class.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const classSchema = new mongoose.Schema({
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",  // Reference to the User model (Student)
+        required: true,
+    },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",  // Reference to the Course model
+        required: true,
+    },
+    tutor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",  // Reference to the User model (Tutor)
+        required: true,
+    },
+    time: {
+        type: Date,  // Class time
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,  // Default to current timestamp
+    },
+});
+
+module.exports = mongoose.model("Class", classSchema);
