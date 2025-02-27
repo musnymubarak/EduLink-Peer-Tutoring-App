45a67f84e567885ec62eaea6bca440a74b0dc274
Disitha <disithar@gmail.com>
2025-02-27 13:50:55 +0530

    Update Google logo import and configure Firebase with environment variables

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -13,6 +13,7 @@ import {
 } from "../../src/firebaseConfig";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import "./css/Login.css";
+import {googlelogo} from "../assets/google.png";
 
 export default function Login() {
   const { setAccountType } = useAccountType();
@@ -182,7 +183,7 @@ export default function Login() {
               className="google-login-btn mt-4 w-full max-w-md py-3 flex items-center justify-center rounded-lg border border-transparent bg-blue-500 text-white"
             >
               <img
-                src="https://developers.google.com/identity/images/g-logo.png"
+                src={{ googlelogo }}
                 alt="Google Logo"
                 style={{ width: 20, marginRight: 10 }}
               />
--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -2,17 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
-/*const firebaseConfig = {
-  apiKey: "AIzaSyBD6dvTxgAUymfFpuWQirMi2PvY-uYkXqA",
-  authDomain: "edulink-4b013.firebaseapp.com",
-  projectId: "edulink-4b013",
-  storageBucket: "edulink-4b013.firebasestorage.app",
-  messagingSenderId: "987278684191",
-  appId: "1:987278684191:web:01fc174f0d41854ad7a52a",
-  measurementId: "G-RVP1EW7DWT",
-};*/
-
+// Debugging: Check if variables are loaded correctly
+console.log("API KEY:", process.env.REACT_APP_FIREBASE_API_KEY);
+console.log("Auth Domain:", process.env.REACT_APP_FIREBASE_AUTH_DOMAIN);
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
